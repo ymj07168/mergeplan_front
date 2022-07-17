6c59e91cf7a6c6f70ff45498383a29d415797cac
이효원 <ymj07168@sookmyung.ac.kr>
2022-07-17 20:10:56 +0900

    first commit

---
+++ src/routes/Detail.js
@@ -0,0 +1,7 @@
+function Detail() {
+    return (
+        <h1>account</h1>
+    );
+}
+
+export default Detail;
---
+++ src/routes/Home.js
@@ -0,0 +1,8 @@
+
+function Home() {
+    return (
+        <h1>mergeplan</h1>
+    );
+}
+
+export default Home;
