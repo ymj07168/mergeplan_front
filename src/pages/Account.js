39e1ff4184112c6da9ec481d5bdded0e95af3b0c
이효원 <ymj07168@sookmyung.ac.kr>
2022-07-21 17:12:12 +0900

    '폴더명 변경'

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -2,5 +2,15 @@ import React from 'react';
 import '../App.css';
 
 export default function Account() {
-    return <h1 className='account'>Account</h1>;
+    return (
+        <>
+            <div>
+                < h1 className='account' >Account</h1 >
+                <p>
+                    이 페이지는 가계부 페이지입니다.
+                    account
+                </p>
+            </div>
+        </>
+    )
 }
