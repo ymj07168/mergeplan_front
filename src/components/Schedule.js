820cd34117cb913ab2d3faea0b969e7876c67ff0
이효원 <ymj07168@sookmyung.ac.kr>
2022-07-23 16:01:27 +0900

    react-big-calendar Test

---
+++ src/components/Schedule.js
@@ -0,0 +1,36 @@
+import moment from "moment";
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+
+function Schedule() {
+    moment.locale('ko-KR');
+    const localizer = momentLocalizer(moment);
+
+    const myEventList = [
+        { start: new Date(), end: new Date(), title: "special event" }
+    ];
+
+    // Calendar.momentLocalizer(moment);
+    return (
+        <div>
+            <Calendar
+                localizer={localizer}
+                events={myEventList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            // step={60}
+            // view="month"
+            // views={["month"]}
+            // min={new Date(2008, 0, 1, 8, 0)}
+            // max={new Date(2008, 0, 1, 17, 0)}
+            // date={new Date(2019, 1, 20)}
+            />
+        </div>
+    )
+}
+
+export default Schedule;
+
--- src/pages/Planner.js
+++ src/pages/Planner.js
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import '../App.css';
+import Schedule from '../components/Schedule';
 
 export default function Planner() {
 
-    const [test, setTest] = useState([]);
-    const getTest = async () => {
-        const json = await (
-            await fetch('')
-        ).json();
-        setTest(json.data.test);
-    };
-    useEffect(() => {
-        getTest();
-    }, []);
-    console.log(test);
+    // const [test, setTest] = useState([]);
+    // const getTest = async () => {
+    //     const json = await (
+    //         await fetch('')
+    //     ).json();
+    //     setTest(json.data.test);
+    // };
+    // useEffect(() => {
+    //     getTest();
+    // }, []);
+    // console.log(test);
 
     return (
         <>
-            <div>
+            <Schedule />
+            {/* <div>
                 < h1 className='planner' >planner</h1 >
                 <p>
                     이 페이지는 플래너 페이지입니다.
                     planner
                 </p>
-            </div>
+            </div> */}
         </>
     );
 }
