d5c18ed8d617d78ffc6cd8ab2db4c0c64b543b4e
dayoung <adask13@naver.com>
2022-08-02 17:03:13 +0900

    footer오류수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,80 +38,81 @@ function Footer() {
                         <h2>Belong To</h2>
                         <p>SookMyung Women's University</p>
                         <p>SOLUX</p>
-                        
-                    <div className='footer-link-items'>
-                        <h2>Contact Us</h2>
-                        <Link to='/'>Contact</Link>
-                        <Link to='/'>Support</Link>
-                        <Link to='/'>Destinations</Link>
-                        <Link to='/'>Sponsorships</Link>
 
+                        <div className='footer-link-items'>
+                            <h2>Contact Us</h2>
+                            <Link to='/'>Contact</Link>
+                            <Link to='/'>Support</Link>
+                            <Link to='/'>Destinations</Link>
+                            <Link to='/'>Sponsorships</Link>
+
+                        </div>
+                    </div>
+                    <div className='footer-link-wrapper'>
+                        <div className='footer-link-items'>
+                            <h2>Videos</h2>
+                            <Link to='/'>Submit Video</Link>
+                            <Link to='/'>Ambassadors</Link>
+                            <Link to='/'>Agency</Link>
+                            <Link to='/'>Influencer</Link>
+                        </div>
+                        <div className='footer-link-items'>
+                            <h2>Social Media</h2>
+                            <Link to='/'>Instagram</Link>
+                            <Link to='/'>Facebook</Link>
+                            <Link to='/'>Youtube</Link>
+                            <Link to='/'>Twitter</Link>
+                        </div>
                     </div>
                 </div>
-                <div className='footer-link-wrapper'>
-                    <div className='footer-link-items'>
-                        <h2>Videos</h2>
-                        <Link to='/'>Submit Video</Link>
-                        <Link to='/'>Ambassadors</Link>
-                        <Link to='/'>Agency</Link>
-                        <Link to='/'>Influencer</Link>
+                <section className="social-media">
+                    <div className="social-media-wrap">
+                        <div className="footer-logo">
+                            <Link to='/' className="social-logo">
+                                MergePlan <i className="fab fa-typo3"></i>
+                            </Link>
+                        </div>
+                        <small className="website-rights">MergePlan © 2022</small>
+                        <div className="social-icons">
+                            <Link className="social-icon-link facebook" to="/"
+                                target="_blank"
+                                aria-label="Facebook"
+                            >
+                                <i className="fab fa-facebook-f"></i>
+                            </Link>
+                            <Link className="social-icon-link instagram" to="/"
+                                target="_blank"
+                                aria-label="Instagram"
+                            >
+                                <i className="fab fa-instagram"></i>
+                            </Link>
+                            <Link className='social-icon-link youtube'
+                                to='/'
+                                target='_blank'
+                                aria-label='Youtube'
+                            >
+                                <i className='fab fa-youtube' />
+                            </Link>
+                            <Link
+                                className='social-icon-link twitter'
+                                to='/'
+                                target='_blank'
+                                aria-label='Twitter'
+                            >
+                                <i className='fab fa-twitter' />
+                            </Link>
+                            <Link
+                                className='social-icon-link twitter'
+                                to='/'
+                                target='_blank'
+                                aria-label='LinkedIn'
+                            >
+                                <i className='fab fa-linkedin' />
+                            </Link>
+                        </div>
                     </div>
-                    <div className='footer-link-items'>
-                        <h2>Social Media</h2>
-                        <Link to='/'>Instagram</Link>
-                        <Link to='/'>Facebook</Link>
-                        <Link to='/'>Youtube</Link>
-                        <Link to='/'>Twitter</Link>
-                    </div>
-                </div>
+                </section>
             </div>
-            <section className="social-media">
-                <div className="social-media-wrap">
-                    <div className="footer-logo">
-                        <Link to='/' className="social-logo">
-                            MergePlan <i className="fab fa-typo3"></i>
-                        </Link>
-                    </div>
-                    <small className="website-rights">MergePlan © 2022</small>
-                    <div className="social-icons">
-                        <Link className="social-icon-link facebook" to="/"
-                            target="_blank"
-                            aria-label="Facebook"
-                        >
-                            <i className="fab fa-facebook-f"></i>
-                        </Link>
-                        <Link className="social-icon-link instagram" to="/"
-                            target="_blank"
-                            aria-label="Instagram"
-                        >
-                            <i className="fab fa-instagram"></i>
-                        </Link>
-                        <Link className='social-icon-link youtube'
-                            to='/'
-                            target='_blank'
-                            aria-label='Youtube'
-                        >
-                            <i className='fab fa-youtube' />
-                        </Link>
-                        <Link
-                            className='social-icon-link twitter'
-                            to='/'
-                            target='_blank'
-                            aria-label='Twitter'
-                        >
-                            <i className='fab fa-twitter' />
-                        </Link>
-                        <Link
-                            className='social-icon-link twitter'
-                            to='/'
-                            target='_blank'
-                            aria-label='LinkedIn'
-                        >
-                            <i className='fab fa-linkedin' />
-                        </Link>
-                    </div>
-                </div>
-            </section>
         </div>
     )
 }
