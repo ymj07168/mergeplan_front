590db30e811a7ca94d62afd4f64ae65756c0f7d8
이효원 <ymj07168@sookmyung.ac.kr>
2022-08-07 10:03:29 +0900

    account 내역 추가 post test

--- src/components/AddScheduleForm.js
+++ src/components/AddScheduleForm.js
@@ -30,6 +30,8 @@ export default function AddScheduleForm(props) {
         axios.post('/auth/planner', data, config)
             .then((result) => {
                 console.log(result)
+                alert('새로운 일정이 추가되었습니다.')
+
             })
             .catch(err => console.log(err))
 
@@ -52,50 +54,53 @@ export default function AddScheduleForm(props) {
         <>
             <form onSubmit={onSdSubmit}>
                 <table className='add-table'>
-                    <tr>
-                        <td>일정</td>
-                        <td colSpan={3}><input type="text" placeholder='제목' size="50" name="title" value={title} onChange={onSdHandler} /></td>
-                    </tr>
-                    <tr>
-                        <td>시작</td>
-                        <td><DatePicker
-                            selected={startDate}
-                            onChange={date => setStartDate(date)}
-                            timeInputLabel="Time:"
-                            dateFormat="MM/dd/yyyy h:mm aa"
-                            showTimeInput
-                            startDate={startDate}
-                        /></td>
-                        <td> ~ 종료</td>
-                        <td><DatePicker
-                            selected={endDate}
-                            onChange={date => setEndDate(date)}
-                            selectsEnd
-                            timeInputLabel="Time: "
-                            dateFormat="MM/dd/yyyy h:mm aa"
-                            showTimeInput
-                            startDate={startDate}
-                            endDate={endDate}
-                        /></td>
-                    </tr>
-                    <tr>
-                        <td>분류</td>
-                        <td colSpan={3}>
-                            <input type="radio" name="Types" value="Work" />Work
-                            <input type="radio" name="Types" value="Party" />Party
-                            <input type="radio" name="Types" value="Shopping" />Shopping
-                            <input type="radio" name="Types" value="Dining" />Dining
-                            <input type="radio" name="Types" value="Trip" />Trip
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>장소</td>
-                        <td colSpan={3}><input type="text" size="50" name="location" /></td>
-                    </tr>
-                    <tr>
-                        <td>설명</td>
-                        <td colSpan={3}><textarea rows="10" cols="50" name="comment"></textarea></td>
-                    </tr>
+                    <thead></thead>
+                    <tbody>
+                        <tr>
+                            <td>일정</td>
+                            <td colSpan={3}><input type="text" placeholder='제목' size="50" name="title" value={title} onChange={onSdHandler} /></td>
+                        </tr>
+                        <tr>
+                            <td>시작</td>
+                            <td><DatePicker
+                                selected={startDate}
+                                onChange={date => setStartDate(date)}
+                                timeInputLabel="Time:"
+                                dateFormat="MM/dd/yyyy h:mm aa"
+                                showTimeInput
+                                startDate={startDate}
+                            /></td>
+                            <td> ~ 종료</td>
+                            <td><DatePicker
+                                selected={endDate}
+                                onChange={date => setEndDate(date)}
+                                selectsEnd
+                                timeInputLabel="Time: "
+                                dateFormat="MM/dd/yyyy h:mm aa"
+                                showTimeInput
+                                startDate={startDate}
+                                endDate={endDate}
+                            /></td>
+                        </tr>
+                        <tr>
+                            <td>분류</td>
+                            <td colSpan={3}>
+                                <input type="radio" name="Types" value="Work" />Work
+                                <input type="radio" name="Types" value="Party" />Party
+                                <input type="radio" name="Types" value="Shopping" />Shopping
+                                <input type="radio" name="Types" value="Dining" />Dining
+                                <input type="radio" name="Types" value="Trip" />Trip
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>장소</td>
+                            <td colSpan={3}><input type="text" size="50" name="location" /></td>
+                        </tr>
+                        <tr>
+                            <td>설명</td>
+                            <td colSpan={3}><textarea rows="10" cols="50" name="comment"></textarea></td>
+                        </tr>
+                    </tbody>
                 </table>
                 <input type="submit" id="btn-add-schedule" value="일정추가" />
             </form>
