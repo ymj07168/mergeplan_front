c0d1e39bb8dca6da77446d144d36861319456421
dayoung <adask13@naver.com>
2022-08-17 03:36:39 +0900

    footer와 home화면 get started버튼

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../App.css';
 import { Button } from './Button'
 import './HeroSection.css';
+import { Link } from 'react-router-dom';
 import imgA from '../images/company.png';
 
 function HeroSection() {
@@ -12,8 +13,8 @@ function HeroSection() {
             <h1>이 돈을 언제 썼지?</h1><br></br>
             <p>더 이상 고민하지 마세요! MergePlan이 다 알려드립니다.</p>
             <div className="hero-btns">
-                <Button className='btns' buttonStyle='btn--outline'
-                    buttonSize='btn--large'>GET STARTED</Button>
+                <Link to="/sign-up"><Button className='btns' buttonStyle='btn--outline'
+                    buttonSize='btn--large'>GET STARTED</Button></Link>
                 <Button className='btns' buttonStyle='btn--primary'
                     buttonSize='btn--large'>WATCH TRAILER <i className='far fa-play-circle' /></Button>
             </div>
